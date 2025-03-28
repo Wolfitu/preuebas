82f3e72007370d97fcab4b473ec5c32cfe4703b8
Wolfitu <wolfitomorning@gmail.com>
2025-03-28 09:04:45 +0100

    Add files via upload

---
+++ Dni.java
@@ -0,0 +1,8 @@
+public class Dni {
+    int numeroDni;
+    static final String LETRAS_DNI="TRWAGMYFPDXBNJZSQVHLCKE";
+    int getDni;
+    String getNif;
+    void setDni(String) throws IllegalArgumentException{ }
+    void setDni(int) throws IllegalArgumentException { }
+}
